2b42fea75b6c982bb23d127f471f838c51d75285
sufei <sufei@weichengya.cn>
2026-01-23 10:12:56 +0800

    绝对路径引用

--- qaction/qicon.py
+++ qaction/qicon.py
@@ -8,6 +8,18 @@ from PySide6.QtWidgets import (
     QStatusBar,
     QToolBar,
 )
+import sys
+import os
+
+# 添加项目根目录到 Python 路径
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 上面一行用下面2行代码一样能实现
+# from pathlib import Path
+# project_root = Path(__file__).parent.parent
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from res.res_helper import get_img_path
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,8 +35,9 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        # icon_path = 
-        button_action = QAction(QIcon("./imgs/open_light_btn.png"), "Your button", self)
+        icon_path = get_img_path('open_light_btn')
+        print(f'imgpath:{icon_path}')
+        button_action = QAction(QIcon(icon_path), "Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.toolbar_button_clicked)
         button_action.setCheckable(True)
